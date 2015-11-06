e6db66a6ec5d236f04b235d70d0263755bf110a6
Calé DeLaura <cdelaura@swatc.edu>
2015-11-06 08:57:20 -0700

    Create bucketlist.cpp
    Created a simple bucket list program just as a side project. Code is far from perfect and has plenty of room for improvement. Feel free to make any changes/suggestions.

---
+++ bucketlist.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	//Bucket List App
+	string listItem1 = "";
+	string listItem2 = "";
+	string listItem3 = "";
+	string listItem4 = "";
+	string listItem5 = "";
+	string listItem6 = "";
+	string listItem7 = "";
+	string listItem8 = "";
+	string listItem9 = "";
+	string listItem10 = "";
+	
+	cout << "Please fill out your bucket list with up to 10 entries." << endl;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem1;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem2;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem3;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem4;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem5;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem6;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem7;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem8;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem9;
+	cout << "Enter bucket list item: " << endl;
+	cin >> listItem10;
+	
+	cout<< "Your bucket list: " << listItem1 << " " << listItem2 << " " << listItem3 << " " << listItem4 << " " << listItem5 << " " << listItem6 << " " << listItem7 << " " << listItem8 << " " << listItem9 << " " << listItem10 << endl;
+	
+	return 0;
+}
