ac45dae2de21ce88ec67073d81dfe9e960fc9c93
Calé DeLaura <cdelaura@swatc.edu>
2015-12-16 08:58:01 -0700

    Create simpeforloop.cpp
    For loop that counts by 20 from 10-100. Displaying 10, 30, 50, 70, 90.

---
+++ simpeforloop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	//Start for loop at 10 as long as number is less than 100 and increase by 20
+	for (int number = 10; number < 100; number += 20)
+	{
+		cout << number << endl;
+		
+	}
+}
